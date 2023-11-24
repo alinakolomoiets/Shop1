94e6642e053c7d12dc4dae7588218679ff2e2a53
opilane <opilane@HP-CZC2349HT3>
2023-11-24 15:45:55 +0200

    Add project files.

---
+++ Models/Graafik.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Graafik
+    {
+        public int Id { get; set; }
+        public DayOfWeek Paev { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh\\:mm}")]
+        public DateTime AvatudAeg { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh\\:mm}")]
+        public DateTime SuletudAeg { get; set; }
+        public int PoodId { get; set; }
+    }
+}
---
+++ Models/Pood.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Models
+{
+    public class Pood
+    {
+        public int Id { get; set; }
+        public string PoodName { get; set; }
+        public string PoodAsukoht { get; set; }
+        public string ContactInfo { get; set; }
+    }
+}
